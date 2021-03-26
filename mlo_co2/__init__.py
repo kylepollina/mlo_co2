3d92b871debf0ed77fe76632c69cad99d95c16ef
kyle pollina <>
2021-03-25 23:39:37 -0500

    Update init

--- mlo_co2/__init__.py
+++ mlo_co2/__init__.py
@@ -2,5 +2,5 @@
 from .mlo_co2 import monthly_mean, annual_mean, annual_mean_increase, weekly_mean
 
 __all__ = [
-    monthly_mean, annual_mean, annual_mean_increase, weekly_mean
+    'monthly_mean', 'annual_mean', 'annual_mean_increase', 'weekly_mean'
 ]
